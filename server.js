c60b7f8b5c1cc13d5e5cb36158488742b781595e
argjend551 <argjend551@hotmail.com>
2022-03-17 18:28:09 +0100

    Added customer model and authentication

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 const express = require("express");
 const server = express();
+server.use(express.json());
+
+// adding session
+session = require("express-session");
+server.use(
+  session({
+    secret: "iejgieohgiehdfgdfgdflg,rrtrp",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
+
+// server start
 server.listen(3000, () => {
   console.log("server started at http://localhost:3000/data");
 });
@@ -7,14 +21,38 @@ server.listen(3000, () => {
 // Use mongoose
 const mongoose = require("mongoose");
 
-// const REST = require("./rest-api.js");
+// Customer model
+const Customer = mongoose.model(
+  "Customer",
+  new mongoose.Schema({
+    firstname: String,
+    lastname: String,
+    email: { type: String, unique: true, required: true, minlength: 8 },
+    password: String,
+    pictureURL: String,
+    cellphone: String,
+    address: {
+      street: String,
+      city: String,
+    },
+    publicEmail: String,
+  })
+);
+// rest api
+const RestCustomer = require("./rest-api/customer.js");
+const RestAuthentication = require("./rest-api/authentication.js");
 
 // Connect to the mongo database atlas
 async function start() {
   await mongoose.connect(
-    "mongodb+srv://Auctionista_Grupp_C:FjsfzO6md5gq6Idk@cluster-auctionista-gru.ql1dv.mongodb.net/Cluster-Auctionista-Grupp-C?retryWrites=true&w=majority"
+    "mongodb+srv://Auctionista_Grupp_C:FjsfzO6md5gq6Idk@cluster-auctionista-gru.ql1dv.mongodb.net/Cluster-Auctionista-Grupp-C?retryWrites=true&w=majority",
+    () => {
+      console.log("MongoDB Connected");
+    },
+    (e) => console.error(e)
   );
   // add REST api
-  // REST();
+  RestCustomer(server, Customer);
+  RestAuthentication(server, Customer);
 }
 start();
