cf5c5512b953b4b5a3621609ea82c46d1becfc5d
argjend551 <argjend551@hotmail.com>
2022-03-17 09:27:45 +0100

    Added Mongoose and connected to the atlas server

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
-const express = require('express');
+const express = require("express");
 const server = express();
-server.listen(3000, () =>{
-console.log('server started at http://localhost:3000/data');
- })
+server.listen(3000, () => {
+  console.log("server started at http://localhost:3000/data");
+});
+
+// Use mongoose
+const mongoose = require("mongoose");
+
+// const REST = require("./rest-api.js");
+
+// Connect to the mongo database atlas
+async function start() {
+  await mongoose.connect(
+    "mongodb+srv://Auctionista_Grupp_C:FjsfzO6md5gq6Idk@cluster-auctionista-gru.ql1dv.mongodb.net/Cluster-Auctionista-Grupp-C?retryWrites=true&w=majority"
+  );
+  // add REST api
+  // REST();
+}
+start();
