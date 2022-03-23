63d446605c39cdc62317020d1e268e8f9328ce78
Sana <72435416+SanaIsShana@users.noreply.github.com>
2022-03-23 11:23:06 +0100

    Added a get for searching auctionItem in a catagory User Stories 15

--- rest-api/bid.js
+++ rest-api/bid.js
@@ -39,6 +39,6 @@ module.exports = function (server, Bid) {
   // Remove the bid
   server.delete("/data/bid/:id", async (request, response) => {
     let result = await Bid.findByIdAndRemove(request.params.id);
-    response.json("Bid " + request.body.auctionItem + " is removed");
+    response.json("One bid is removed");
   });
 };
